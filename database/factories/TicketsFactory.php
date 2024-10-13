45b1cceb817187999afd7f97f225420ebea37094
Rj <rjbahoyo25@gmail.com>
2024-10-13 16:18:52 +0800

    ticket_factory_updated

--- database/factories/TicketsFactory.php
+++ database/factories/TicketsFactory.php
@@ -18,6 +18,26 @@ class TicketsFactory extends Factory
     {
         return [
             'user_code' => $this->faker->bothify('USR###??'),
+            'date_entered' => $dateEntered = $this->faker->dateTimeBetween('September 1st ' . now()->year, 'now'),
+            'transaction_id' => $this->faker->numberBetween(1, 3), // Assuming transaction IDs are 1, 2, or 3
+            'reference_no' => $this->faker->unique()->regexify('[A-Z0-9]{8}'),
+            'remarks' => $this->faker->sentence(), // Ensure remarks are always generated
+            'expiry_date_time' => $this->faker->dateTimeBetween($dateEntered, 'December 31, ' . now()->year), // Expiry date within this year
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
+
+
+/*
+class TicketsFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'user_code' => $this->faker->bothify('USR###??'),
             'date_entered' => $this->faker->date('Y-m-d', now()), // Ensures date is in the current year
             'transaction_id' => $this->faker->numberBetween(1, 3), // Assuming transaction IDs are 1, 2, or 3
             'reference_no' => $this->faker->unique()->regexify('[A-Z0-9]{8}'),
@@ -28,3 +48,4 @@ class TicketsFactory extends Factory
         ];
     }
 }
+*/
